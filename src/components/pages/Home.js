39db7b9835a62759ca9f46b3a8e7a187ad13e7d6
Mujinga Williams <mujingawilliams@Mujingas-MBP.attlocal.net>
2021-05-20 06:19:13 -0400

    home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,17 +1,56 @@
 import React from "react";
+import ControlledCarousel from 'react-bootstrap/ControlledCarousel';
 
-function Home() {
+function ControlledCarousel() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
   return (
-    <div>
-      <div style={{ textAlign: "center" }}>
-        <h1>Dynamic Context API Practice!</h1>
-        <h3 className="mb-4">Press a button to get an alert!</h3>
-      </div>
-      <div style={{ margin: "0 auto" }}>
-        
-      </div>
-    </div>
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=First slide&bg=373940"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>Hello </h3>
+          <p>My name is Mujinga Williams</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=282c34"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Welcome</h3>
+          <p>To my Portfolio</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Thank you</h3>
+          <p>
+            For visiting my site
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 }
 
-export default Home;
+render(<ControlledCarousel />);
+
+
